7902ed5ab8968bbce3860a50eb6591f54381f22c
Shivesh-Kaundinya <54278503+Shivesh-Kaundinya@users.noreply.github.com>
2021-12-25 16:19:45 +0530

    added firebase

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCpM-gK_SSR8j7AniqNZNx2ptpz65sIB9g",
+    authDomain: "mess-management-isd.firebaseapp.com",
+    projectId: "mess-management-isd",
+    storageBucket: "mess-management-isd.appspot.com",
+    messagingSenderId: "994869481650",
+    appId: "1:994869481650:web:0c99d1afea9399863bb9ab"
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export const { app, firestore, auth };
